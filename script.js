00c59fa3a456e6cd26a236d9a5d187a8f7d59a67
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-05-28 19:18:31 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  // Red
+  $('.red').click(function () {
+    $('body').css('background-color', 'red');
+  });
+  // Red
+  $('.yellow').click(function () {
+    $('body').css('background-color', 'yellow');
+  });
+  // Red
+  $('.green').click(function () {
+    $('body').css('background-color', 'green');
+  });
+  // Red
+  $('.blue').click(function () {
+    $('body').css('background-color', 'blue');
+  });
+  // Red
+  $('.cyan').click(function () {
+    $('body').css('background-color', 'cyan');
+  });
+  // Red
+  $('.purple').click(function () {
+    $('body').css('background-color', 'purple');
+  });
+  // Red
+  $('.orange').click(function () {
+    $('body').css('background-color', 'orange');
+  });
+  // Skyblue
+  $('.skyblue').click(function () {
+    $('body').css('background-color', 'skyblue');
+  });
+});
